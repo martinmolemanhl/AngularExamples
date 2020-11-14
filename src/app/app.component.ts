c3d335765f30bb44f36dd5284f93f466eecf6e5f
martinmolemanhl <martin.molema@nhlstenden.com>
2020-11-14 15:45:08 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  public fruit: string[] = [];
+
+  constructor() {
+    this.fruit.push("Apple");
+    this.fruit.push("Orange");
+    this.fruit.push("Banana");
+    this.fruit.push("Pineapple");
+  }
 }
